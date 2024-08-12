b8f7757d21ad30313c0de600333f7018f3dbca96
jotavare <jpoliveira.mm@gmail.com>
2024-08-13 00:23:53 +0100

    Format document

--- sketches/pir_sensor/pir_sensor.ino
+++ sketches/pir_sensor/pir_sensor.ino
@@ -1,22 +1,19 @@
-int ledPin = 13;  // LED on Pin 13 of Arduino
-int pirPin = 7; // Input for HC-S501
+int ledPin = 13; // LED on Pin 13 of Arduino
+int pirPin = 7;  // Input for HC-S501
 
 int pirValue; // Place to store read PIR Value
 
+void setup()
+{
 
-void setup() {
-  
   pinMode(ledPin, OUTPUT);
   pinMode(pirPin, INPUT);
- 
+
   digitalWrite(ledPin, LOW);
-  
-  
-  
 }
 
-void loop() {
+void loop()
+{
   pirValue = digitalRead(pirPin);
   digitalWrite(ledPin, pirValue);
-
 }
